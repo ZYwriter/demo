a00bd197932454863b921b161ca8b50db7e08d7e
Smiledemon <790333234@qq.com>
2018-01-30 10:37:06 +0800

    Test

---
+++ src/main/java/xceder/Test.java
@@ -0,0 +1,15 @@
+package xceder;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ *
+ * @author: Mac
+ * Date: 2018-01-30
+ * Time: 上午10:36
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("say hello!");
+    }
+}
