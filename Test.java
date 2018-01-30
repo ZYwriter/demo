ec6660bb72542224ab0f563051eb683b5c8db97c
Smiledemon <34064939+Smiledemon@users.noreply.github.com>
2018-01-30 11:26:04 +0800

    Update Test.java

--- Test.java
+++ Test.java
@@ -3,5 +3,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello Worsasfdfsdfsld!" );
+        System.out.println( "Hello Worsasfdfsdfsld!" );
     }
 }
