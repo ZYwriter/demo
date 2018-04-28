0c32771c0c0bce3e8c8a973ebd33db5e83e0281f
Smiledemon <34064939+Smiledemon@users.noreply.github.com>
2018-04-28 16:54:32 +0800

    Revert "Update Test.java"

--- Test.java
+++ Test.java
@@ -4,6 +4,5 @@ public class App
     {
         System.out.println( "Hello Worsasfdfsdfsld!" );
         System.out.println( "Hello Worsasfdfsdfsld!" );
-        System.out.println( "Hello Worsasfdfsdfsld!" );System.out.println( "Hello Worsasfdfsdfsld!" );
     }
 }
